8065ebb85d47f56623a7764692f65f6cc04cfde3
Jukka Aho <ahojukka5@gmail.com>
2020-12-11 22:15:06 +0700

    Updates

---
+++ precache-manifest.a29b8f4f86267466575d2644d2ef1433.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e41b06b4906bc4f742d3ca7138d298f",
+    "url": "/anecdotes/index.html"
+  },
+  {
+    "revision": "796a6bde2eb591adff35",
+    "url": "/anecdotes/static/css/2.f23f070e.chunk.css"
+  },
+  {
+    "revision": "796a6bde2eb591adff35",
+    "url": "/anecdotes/static/js/2.067bf1a5.chunk.js"
+  },
+  {
+    "revision": "d09b9a4c38d03daa6dc4",
+    "url": "/anecdotes/static/js/main.e64e9b11.chunk.js"
+  },
+  {
+    "revision": "ac52e3b40e865ddb1acb",
+    "url": "/anecdotes/static/js/runtime-main.fb4979ae.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/anecdotes/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/anecdotes/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/anecdotes/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/anecdotes/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/anecdotes/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/anecdotes/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/anecdotes/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/anecdotes/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/anecdotes/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/anecdotes/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/anecdotes/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/anecdotes/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/anecdotes/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/anecdotes/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/anecdotes/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/anecdotes/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
